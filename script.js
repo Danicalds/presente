a721d78855b26fde4e6ed1e4a47b2d76895463ae
Danicalds <129795062+Danicalds@users.noreply.github.com>
2023-05-12 19:27:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoMudarImagem = document.querySelector("#botaoMudarImagem");
+const figura = document.querySelector("#imagem");
+
+    botaoMudarImagem.addEventListener("click", function(){
+
+    imagem.setAttribute("src","https://static-cse.canva.com/blob/612076/ModelodeCartodeHomenagemparaMes.png");
+
+});
